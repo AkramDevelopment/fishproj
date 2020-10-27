715889c936da942964224edab7c14654accd1636
akram mohammed <akram.development50@gmail.com>
2020-10-27 01:04:06 -0500

    asd

--- buttonServer.py
+++ buttonServer.py
@@ -1,15 +1,15 @@
 import requests
 import RPi.GPIO as gpio
 
+gpio.setwarnings(False)
+gpio.setmode(gpio.BOARD)
+gpio.setup(16,gpio.IN,pull_up_down=gpio.PUD_DOWN)
 
 
-while True:
+while True: 
 
-     
-    gpio.setwarnings(False)
-    gpio.setmode(gpio.BOARD)
-    gpio.setup(16,gpio.IN,pull_up_down=gpio.PUD_DOWN)
-    if gpio.input(16) == gpio.HIGH:
+    if gpio.input(16 == gpio.HIGH):
+
         print("Button pushed")
     
 
