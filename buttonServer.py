b78b05e90211dd32295f4ec9571297a1f752df6a
akram mohammed <akram.development50@gmail.com>
2020-10-27 01:20:19 -0500

    update

--- buttonServer.py
+++ buttonServer.py
@@ -4,17 +4,16 @@ import RPi.GPIO as gpio
 gpio.setwarnings(False)
 gpio.setmode(gpio.BOARD)
 gpio.setup(36,gpio.IN,pull_up_down=gpio.PUD_UP)
-btn_pin = 36
+
+
 
 try:
 
     while True:
 
-        button = gpio.input(btn_pin)
-        if btn_pin == False:
-
+        button = gpio.input(36)
+        if button == False:
             print("Button pressed, fiannly be working")
-            btn_pin = True
 
 except Exception as e:
     gpio.cleanup()
