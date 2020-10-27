2fcff169c91bcb07849bf6c929cd8e7af65e94f5
akram mohammed <akram.development50@gmail.com>
2020-10-27 01:18:49 -0500

    asd

--- buttonServer.py
+++ buttonServer.py
@@ -5,15 +5,18 @@ gpio.setwarnings(False)
 gpio.setmode(gpio.BOARD)
 gpio.setup(36,gpio.IN,pull_up_down=gpio.PUD_UP)
 
+btn_pin = 36
 
 
 try:
 
     while True:
 
-        button = gpio.input(36)
-        if button == False:
-            print("Button pressed, fiannly be workin")
+        button = gpio.input(btn_pin)
+        if btn_pin == False:
+
+            print("Button pressed, fiannly be working")
+            btn_pin == True
 
 except Exception as e:
     gpio.cleanup()
