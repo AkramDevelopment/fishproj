146926c4525d859111059de5a289bf9e9d15c164
akram mohammed <akram.development50@gmail.com>
2020-10-27 00:31:59 -0500

    update

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,request,redirect,url_for
+from gpiozero import Button
 import logging
 import time
 import RPi.GPIO as gpio
@@ -10,23 +11,29 @@ buzzerMotor = 12
 button  = 16 
 
 
+
+
+
 gpio.setwarnings(False) # Ignore warning for now
 gpio.setmode(gpio.BOARD) # Use physical pin numbering
 gpio.setup(button, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
-try:
-    while True: 
-        if gpio.input(button):
 
-            print("Button Activated")
-        else:
+def pull_lever():
 
-            print("no")
-except: 
-    print("There was an error with the code GET TO DEBUGGIGN")
+    gpio.setmode(gpio.BCM)
+    gpio.setup(buzzerMotor,gpio.OUT)
+    gpio.output(buzzerMotor,False)
+    gpio.output(buzzerMotor, True)
+    gpio.setup(buzzerMotor,gpio.OUT)
+    time.sleep(.3)
+    gpio.output(buzzerMotor,False)
+    time.sleep(1)
+    print("success")
+    return ({"success": "Fish are beeing fed, they are full!"})
 
-
-
+btn = Button(16)
+btn.when_pressed = pull_lever()
 
 
 
